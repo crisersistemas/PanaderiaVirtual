02ebec38d3e5f31e904d09f0af82f5b97bb41ea5
Crisanto jose serrano <crisanto.serrano@gmail.com>
2020-01-09 21:33:58 +0000

    probando imagen

--- src/js/component/shopping-Car.js
+++ src/js/component/shopping-Car.js
@@ -1,12 +1,12 @@
 import React from "react";
 export const ShoppingCar = () => {
 	return (
-		<div className="card" style="width: 18rem;">
-			<img src="../../DSC_0069.JPG" className="card-img-top" alt="..." />
+		<div className="card" style={{ width: 18 + "rem" }}>
+			<img src="../../img/DC_0069.jpg" className="card-img-top" alt="..." />
 			<div className="card-body">
 				<h5 className="card-title">Card title</h5>
 				<p className="card-text">
-					Some quick example text to build on the card title and make up the bulk of the card's content.
+					{"Some quick example text to build on the card title and make up the bulk of the card's content."}
 				</p>
 			</div>
 			<ul className="list-group list-group-flush">
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "../../styles/home.scss";
-import { ShopingCar } from "../component/shopping-Car";
+import { ShoppingCar } from "../component/shopping-Car";
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<ShopingCar />
+		<ShoppingCar />
 	</div>
 );
